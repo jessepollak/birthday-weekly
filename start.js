f17ca18f432e888f1ed6a2406f4a5e3b90a53a17
Jesse Pollak <jpollak@coinbase.com>
2019-11-28 12:09:40 -0500

    scheduled

---
+++ start.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+if (process.env.SCHEDULED_WORKER) {
+  const scheduled = require('./scheduled')
+
+
+  const scheduledRouter = scheduled.createRouter()
+
+  app.use('/scheduled', scheduledRouter)
+
+} else {
+  const api = require('./api')
+  api.initialize(app)
+}
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Hello world listening on port', port);
+});
+
+
+
+
