a75d1c097810c5920b2ca1d8b38d93f65e2ffe48
Jesse Pollak <jpollak@coinbase.com>
2019-12-30 18:57:47 -0800

    log body

--- src/routes/tasks/sendWeeklyEmail.ts
+++ src/routes/tasks/sendWeeklyEmail.ts
@@ -3,6 +3,8 @@ import email from '../../lib/email'
 import { ContactRepository } from '../../lib/models/Contact'
 
 export default async function sendWeeklyEmail(req, res) {
+  console.log(req.body)
+
   const user = await UserRepository.find(req.body.userId)
   const upcomingContacts = await ContactRepository.fetchContactsWithUpcomingBirthdays(user)
 
